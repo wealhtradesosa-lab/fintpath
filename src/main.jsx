d18ddf4e9b093ced4e6deadd3f94974e58ad27cc
FINPATH Deploy <deploy@finpath.app>
2026-03-30 21:45:44 +0000

    🔄 Trigger Netlify deploy V9.6

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-// FINPATH V6.2 build 20260324-1500
+// FINPATH V9.6 build 20260330-2145
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
